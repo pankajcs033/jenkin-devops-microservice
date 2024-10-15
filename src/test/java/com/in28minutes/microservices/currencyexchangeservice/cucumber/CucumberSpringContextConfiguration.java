e5b2247f5eb0e61dd480048a73d2dd55d97b215c
pankaj <sainikumarpankaj786@gmail.com>
2024-10-15 09:40:29 +0530

    updated java test files

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.in28minutes.microservices.currencyexchangeservice.CurrencyExchangeServiceApplicationH2;
 
+import io.cucumber.java.Before;
+
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = CurrencyExchangeServiceApplicationH2.class, loader = SpringBootContextLoader.class)
 public class CucumberSpringContextConfiguration {
